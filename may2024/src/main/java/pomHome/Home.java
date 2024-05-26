32373a2fcd793ff1bb41ca7117c212e8d9e34c49
spselenim <pawal.satish@gmail.com>
2024-05-26 16:18:34 +0530

    first commit

---
+++ may2024/src/main/java/pomHome/Home.java
@@ -0,0 +1,45 @@
+package pomHome;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Home {
+	public WebDriver driver = null;
+
+	public String First = "fname";
+	public String Last = "lname";
+
+	public Home(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	
+	public WebElement getElementFirstName()
+	{
+		WebElement fname1 = driver.findElement(By.id(First));
+		return fname1;
+	}
+
+
+	public WebElement getElementLastName()
+	{
+		WebElement last1 = driver.findElement(By.id(Last));
+		return last1;
+	}
+	public void enterFirstDataIntoString(String Data)
+	{
+		getElementFirstName().sendKeys(Data);;
+	
+	}
+	
+	public void enterLastDataIntoString(String Data)
+	{
+		getElementLastName().sendKeys(Data);;
+	
+	}
+	
+	
+	
+	
+}
